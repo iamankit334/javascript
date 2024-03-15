4f49d3b406afa2d0ad544ea2dbf7fb878fa35a3c
iamankit334 <117710749+iamankit334@users.noreply.github.com>
2024-03-15 07:55:32 +0000

    datatype

--- basic_01/conversion.js
+++ basic_01/conversion.js
@@ -18,3 +18,18 @@ console.log(typeof(convert));
 // all string are converted to true expect if its empty ""
 // all number are converted to true expect 0 which give false;
 // null and undefined gives false
+
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
