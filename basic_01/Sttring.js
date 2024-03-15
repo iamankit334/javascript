eaae67d0003722d0b3e04aa88cd31eab30a90328
iamankit334 <117710749+iamankit334@users.noreply.github.com>
2024-03-15 13:59:49 +0000

    String and its function

---
+++ basic_01/Sttring.js
@@ -0,0 +1,21 @@
+// String in j s
+// two way declartion
+let name = "Ankit" ;  // normal
+let age = new String("21");  // using class  it also store the strig in key value pair
+console.log(`the first letter in my name is  ${name[0]}`);
+
+console.log(`My name  is ${name} and i am ${age} year old `);  // usefull in editing the final result
+// function of String that are present by default 
+console.log(name.length);
+console.log(name.toUpperCase());
+console.log(name.charAt(3));
+console.log(name.indexOf('k'));
+console.log(name.substring(2));
+
+// slice 
+// split
+// substring
+//endwith return boolean values
+//
+
+
