232144811324c56794d4f589cfa0a21ecc9a0c80
iamankit334 <117710749+iamankit334@users.noreply.github.com>
2024-03-15 06:40:47 +0000

    varible and const

---
+++ basic_01/varible.js
@@ -0,0 +1,11 @@
+const accountid = 14554;
+// const is a datatype that is define only once and its value is final
+let accountgmail = "iamjv@gmail.com"; // we uses let know a days 
+var accountname = "ankit";
+let accounttype;
+/*
+perfer not to use the var because 
+of issue in block scope and function scope
+
+*/
+console.table([accountgmail,accountid,accountname,accounttype]);
