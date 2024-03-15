9c9a480343da6bad757e98511ee545787908e3db
iamankit334 <117710749+iamankit334@users.noreply.github.com>
2024-03-15 07:06:01 +0000

    datatypes and ecms

---
+++ basic_01/datatype.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version 
+// alert(3+5)  // we are using node js not web to excuting propose::
+console.log(3+3);
+console.log("Ankit"); // alway keep in mind for the code readiblity 
+
+// datatype 
+/*
+number  range 2^53
+big int 
+string ""
+boolean = true /false
+null  standalone value
+undefineed
+symbol => unique
+
+object 
+
+*/
+console.log(typeof("ankit"));
+console.log(typeof(123));
+console.log(typeof(12/34/3));
+console.log(typeof(null)); // null type is object 
+console.log(typeof(undefined)); // undefined type is undefined
+// check
